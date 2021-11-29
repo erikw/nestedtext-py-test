3c1975d283a20fe5d08d2eed7d1d3bfeaff0d285
Erik Westrup <erik.westrup@gmail.com>
2021-11-29 12:27:17 +0100

    Add test case from file

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # NestedText Python Testing
 Testing of the Python library
+
+## Setup
+
+```console
+$ poetry install
+$ poetry run main
+```
---
+++ nestedtext_py_test/__main__.py
@@ -0,0 +1,25 @@
+import sys
+from pprint import pprint
+
+import nestedtext as nt
+
+
+def test_from_file():
+    addressbook = {}
+    try:
+        addressbook = nt.load("conf/addressbook.nt", top="dict")
+    except nt.NestedTextError as e:
+        e.terminate()
+    except OSError as e:
+        print(e, file=sys.stderr)
+        sys.exit(1)
+    pprint(addressbook["president"]["name"])
+    pprint(type(addressbook["president"]["name"]))
+
+
+def main():
+    test_from_file()
+
+
+if __name__ == "__main__":
+    sys.exit(main())
